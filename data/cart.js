d7388cc1ba916167ebe0574d8cdb4c051653b300
shanmukh <shanmukhanagasaineduri@gmail.com>
2025-12-12 10:52:39 +0530

    loading from the local storage if cart empty giving a fixed value

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart=[{//these are the sample values for the development
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+cart=[{//these are the sample values for the development
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
 
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));//takes two string whatever we want to save and the data we want to save as strig
+}
 
 export function addTOcart(productId){
   let matchingItem;
@@ -23,6 +31,7 @@ export function addTOcart(productId){
     quantity:1
   });
   }
+  saveToStorage();
 }
 
 
@@ -32,6 +41,6 @@ export function removeFromCart(productId){
         cart.splice(i,1);
       }
     }
-  
+  saveToStorage();
 }
 
